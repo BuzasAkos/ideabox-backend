f88cf11cce90b62de1bd20a3458d7e9be9db8b6f
Ákos Buzás <buzas.akos@telekom.hu>
2025-01-29 17:08:28 +0100

    Update idea.entity.ts

--- src/idea/entities/idea.entity.ts
+++ src/idea/entities/idea.entity.ts
@@ -1,45 +1,111 @@
-import { ObjectId } from "mongodb";
+import { ObjectId } from 'mongodb';
+import { Entity, Column, ObjectIdColumn } from 'typeorm';
 
+@Entity('ideas')
 export class Idea {
+
+    @ObjectIdColumn()
     _id: ObjectId;
+
+    @Column()
     title: string;
+
+    @Column()
     description?: string;
+
+    @Column()
     voteCount: number;
+
+    @Column('json')
     votes: Vote[];
+
+    @Column('json')
     comments: Comment[];
+
+    @Column()
     status: string;
-    history: IdeaHistory[];
+
+    @Column('json')
+    history?: IdeaHistory[];
+
+    @Column()
     createdBy: string;
+
+    @Column()
     createdAt: Date;
+
+    @Column()
     modifiedBy: string;
+
+    @Column()
     modifiedAt: Date;
+
+    @Column()
     boolId: boolean;
 }
 
 export class Vote {
+    
+    @Column()
     id: string;
+
+    @Column()
     createdBy: string;
+
+    @Column()
     createdAt: Date;
+
+    @Column()
     modifiedBy: string;
+
+    @Column()
     modifiedAt: Date;
+
+    @Column()
     boolId: boolean;
 }
 
 export class Comment {
+    
+    @Column()
     id: string;
+
+    @Column()
     text: string;
+
+    @Column()
     createdBy: string;
+
+    @Column()
     createdAt: Date;
+
+    @Column()
     modifiedBy: string;
+
+    @Column()
     modifiedAt: Date;
+
+    @Column()
     boolId: boolean;
 }
 
 export class IdeaHistory {
+    
+    @Column()
     id: string;
+
+    @Column()
     title?: string;
+
+    @Column()
     description?: string;
+
+    @Column()
     status?: string;
+
+    @Column()
     createdBy: string;
+
+    @Column()
     createdAt: Date;
 }
